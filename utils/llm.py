0901a088c1520faafa1774058d4eb8f512ade136
NaughtyRider <naughtyagrawal111@gmail.com>
2026-05-17 22:21:15 +0530

    streamlit version

--- utils/llm.py
+++ utils/llm.py
@@ -1,13 +1,18 @@
 import os
+import streamlit as st
 
 from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
-
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+try:
+    client = Groq(
+        api_key=os.getenv("GROQ_API_KEY")
+    )
+except Exception as e:
+    client = Groq(
+        api_key=st.secrets["GROQ_API_KEY"]
+    )
 
 def analyze_resume(prompt):
 
